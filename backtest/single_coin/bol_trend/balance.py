0f7616b363885c2edcbfd8db5f402d1036e935d1
Jackseg <jackseg@bluewin.ch>
2023-01-14 15:07:39 +0100

    colab

--- backtest/single_coin/bol_trend/balance.py
+++ backtest/single_coin/bol_trend/balance.py
@@ -5,30 +5,23 @@ import matplotlib.pyplot as plt
 #matplotlib.use("Agg")
 import pandas as pd
 
-# Open file in read mode
 with open("balance_history.txt", "r") as f:
     data = f.read()
 
-# Split data into lines
 lines = data.split("\n")
 records = [line.split(";") for line in lines]
-
-# Filter records that have the correct number of columns
 records = [record for record in records if len(record) == 2]
 
 df = pd.DataFrame(records, columns=["time", "balance"])
-
-# Convert "time" column to datetime and "balance" column to float
 df["time"] = pd.to_datetime(df["time"], format="%d/%m/%Y %H:%M:%S")
 df["balance"] = df["balance"].astype(float)
 
-# Calculate final balance and difference with 5021
 start_balance = 5021
 final_balance = df["balance"].iloc[-1]
 difference = final_balance - start_balance
 percentage_difference = (difference / start_balance) * 100
 
-# Filter data from specified start time
+# Commencer le graphique depuis cette date
 start_time = pd.to_datetime("15/12/2022")
 df_filtered = df[df["time"] >= start_time]
 
@@ -39,19 +32,15 @@ plt.plot([df_filtered["time"].min(), df_filtered["time"].max()], [start_balance,
 plt.fill_between(df_filtered["time"], df_filtered["balance"], start_balance, where=(df_filtered["balance"] < start_balance), color='red', alpha=0.2)
 plt.fill_between(df_filtered["time"], df_filtered["balance"], start_balance, where=(df_filtered["balance"] >= start_balance), color='green', alpha=0.2)
 
-
-# Add text to graph
+# Gain ou pertes affiché en haut à droite
 if percentage_difference >= 0:
     color = "green"
 else:
     color = "red"
 plt.text(df_filtered["time"].max(), final_balance, f"{final_balance:.2f}\n ({percentage_difference:.2f}%)", ha='right', va='center', color=color)
 
-# Add legend and title
 plt.legend()
 plt.title("Balance history")
 
-# Show graph
 plt.savefig("balance.png", dpi = 300)
-
 plt.show()
