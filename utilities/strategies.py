c000291d311ff17330bb7ab5df6718fdff4c12fb
Jackseg <jackseg@bluewin.ch>
2023-03-10 15:01:34 +0100

    super reversal update

--- utilities/strategies.py
+++ utilities/strategies.py
@@ -2,6 +2,7 @@ import sys
 sys.path.append('..')
 from utilities.custom_indicators import get_n_columns
 from utilities.backtesting import get_metrics
+from utilities.custom_indicators import SuperTrend
 from utilities.VaR import ValueAtRisk
 import pandas as pd
 pd.options.mode.chained_assignment = None  # default='warn'
@@ -767,4 +768,307 @@ class BollingerTrendMulti():
             "wallet": wallet,
             "trades": df_trades,
             "days": df_days
-        }  
+        } 
+        
+class super_reversion_strat():
+    def __init__(
+        self,
+        df_list,
+        oldest_pair,
+        parameters_obj,
+        type=["long"],
+    ):
+        self.df_list = df_list
+        self.oldest_pair = oldest_pair
+        self.parameters_obj = parameters_obj
+        self.use_long = True if "long" in type else False
+        self.use_short = True if "short" in type else False
+        
+    def populate_indicators(self, show_log=False):
+        # -- Clear dataset --
+        for pair in self.df_list:
+            df = self.df_list[pair]
+            params = self.parameters_obj[pair]
+            df.drop(columns=df.columns.difference(['open','high','low','close','volume']), inplace=True)
+            
+            # -- Populate indicators --
+            super_trend = SuperTrend(
+                df['high'], 
+                df['low'], 
+                df['close'], 
+                params["st_short_atr_window"], 
+                params["st_short_atr_multiplier"]
+            )
+            
+            df['super_trend_direction'] = super_trend.super_trend_direction()
+            df['ema_short'] = ta.trend.ema_indicator(close=df['close'], window=params["short_ema_window"])
+            df['ema_long'] = ta.trend.ema_indicator(close=df['close'], window=params["long_ema_window"])
+            
+            df = get_n_columns(df, ["super_trend_direction", "ema_short", "ema_long"], 1)
+            
+            self.df_list[pair] = df
+            # -- Log --
+            if(show_log):
+                print(self.df_list[self.oldest_pair])
+                
+        return self.df_list[self.oldest_pair]
+    
+    def populate_buy_sell(self, show_log=False): 
+        data_open_long = []
+        data_close_long = []
+        data_open_short = []
+        data_close_short = []
+
+        for pair in self.df_list:
+            params = self.parameters_obj[pair]
+            df = self.df_list[pair]
+            # -- Initiate populate --
+            df["open_long_limit"] = False
+            df["close_long_limit"] = False
+            df["open_short_limit"] = False
+            df["close_short_limit"] = False
+            df["pair"] = pair
+            df["null"] = np.nan
+            
+            if self.use_long:
+                # -- Populate open long limit --
+                df.loc[
+                    (df['n1_ema_short'] >= df['n1_ema_long']) 
+                    & (df['n1_super_trend_direction'] == True) 
+                    & (df['n1_ema_short'] > df['low']) 
+                    , "open_long_limit"
+                ] = True
+                
+                # -- Populate close long limit --
+                df.loc[
+                    ((df['n1_ema_short'] <= df['n1_ema_long'])
+                    | (df['n1_super_trend_direction'] == False))
+                    & (df['n1_ema_short'] < df['high'])
+                    , "close_long_limit"
+                ] = True
+
+            if self.use_short:
+                # -- Populate open short limit --
+                df.loc[
+                    (df['n1_ema_short'] <= df['n1_ema_long']) 
+                    & (df['n1_super_trend_direction'] == False) 
+                    & (df['n1_ema_short'] < df['high']) 
+                    , "open_short_limit"
+                ] = True
+                
+                # -- Populate close short market --
+                df.loc[
+                    ((df['n1_ema_short'] >= df['n1_ema_long']) 
+                    | (df['n1_super_trend_direction'] == True))
+                    & (df['n1_ema_short'] > df['low']) 
+                    , "close_short_limit"
+                ] = True
+                
+            # -- Populate pair list per date (do not touch)--
+            data_open_long.append(
+                df.loc[
+                (df['open_long_limit']  == True) 
+                ]['pair']
+            )
+            data_close_long.append(
+                df.loc[
+                (df['close_long_limit']  == True) 
+                ]['pair']
+            )
+            data_open_short.append(
+                df.loc[
+                (df['open_short_limit']  == True) 
+                ]['pair']
+            )
+            data_close_short.append(
+                df.loc[
+                (df['close_short_limit']  == True) 
+                ]['pair']
+            )
+
+        data_open_long.append(self.df_list[self.oldest_pair]['null'])
+        data_close_long.append(self.df_list[self.oldest_pair]['null'])
+        data_open_short.append(self.df_list[self.oldest_pair]['null'])
+        data_close_short.append(self.df_list[self.oldest_pair]['null'])
+        df_open_long = pd.concat(data_open_long, axis=1)
+        df_open_long['combined']= df_open_long.values.tolist()
+        df_open_long['combined'] = [[i for i in j if i == i] for j in list(df_open_long['combined'])]
+        df_close_long = pd.concat(data_close_long, axis=1)
+        df_close_long['combined']= df_close_long.values.tolist()
+        df_close_long['combined'] = [[i for i in j if i == i] for j in list(df_close_long['combined'])]
+        df_open_short = pd.concat(data_open_short, axis=1)
+        df_open_short['combined']= df_open_short.values.tolist()
+        df_open_short['combined'] = [[i for i in j if i == i] for j in list(df_open_short['combined'])]
+        df_close_short = pd.concat(data_close_short, axis=1)
+        df_close_short['combined']= df_close_short.values.tolist()
+        df_close_short['combined'] = [[i for i in j if i == i] for j in list(df_close_short['combined'])]
+        self.open_long_obj = df_open_long['combined']
+        self.close_long_obj = df_close_long['combined']
+        self.open_short_obj = df_open_short['combined']
+        self.close_short_obj = df_close_short['combined']
+        
+        # -- Log --
+        if(show_log):
+            print("Open LONG length on oldest pair :",len(self.df_list[self.oldest_pair].loc[self.df_list[self.oldest_pair]["open_long_limit"]==True]))
+            print("Close LONG length on oldest pair :",len(self.df_list[self.oldest_pair].loc[self.df_list[self.oldest_pair]["close_long_limit"]==True]))
+            print("Open SHORT length on oldest pair :",len(self.df_list[self.oldest_pair].loc[self.df_list[self.oldest_pair]["open_short_limit"]==True]))
+            print("Close SHORT length on oldest pair :",len(self.df_list[self.oldest_pair].loc[self.df_list[self.oldest_pair]["close_short_limit"]==True]))
+        
+        return self.df_list[self.oldest_pair]
+    
+    def run_backtest(self, initial_wallet=1000, return_type="metrics"):
+        df_ini = self.df_list[self.oldest_pair][:]
+        wallet = initial_wallet
+        usd_remaining = initial_wallet
+        usd_pct_remaining = 1
+        maker_fee = 0.00017
+        taker_fee = 0.00051
+        trades = []
+        days = []
+        current_day = 0
+        previous_day = 0
+        current_positions = {}
+        
+        for index, row in df_ini.iterrows():
+            # -- Add daily report --
+            current_day = index.day
+            if previous_day != current_day:
+                temp_wallet = wallet
+                for pos in current_positions:
+                    actual_row = self.df_list[pos].loc[index]
+                    if current_positions[pos]['side'] == "LONG":
+                        close_price = actual_row['close']
+                        trade_result = (close_price - current_positions[pos]['price']) / current_positions[pos]['price']
+                        close_size = current_positions[pos]['size'] + current_positions[pos]['size']  * trade_result
+                        fee = close_size * taker_fee
+                        temp_wallet += close_size - current_positions[pos]['size'] - fee
+                    elif current_positions[pos]['side'] == "SHORT":
+                        close_price = actual_row['close']
+                        trade_result = (current_positions[pos]['price'] - close_price) / current_positions[pos]['price']
+                        close_size = current_positions[pos]['size'] + current_positions[pos]['size']  * trade_result
+                        fee = close_size * taker_fee
+                        temp_wallet += close_size - current_positions[pos]['size'] - fee
+                    
+                days.append({
+                    "day":str(index.year)+"-"+str(index.month)+"-"+str(index.day),
+                    "wallet":temp_wallet,
+                    "price":row['close']
+                })
+            previous_day = current_day 
+            
+            # Sell
+            close_long_row = self.close_long_obj.loc[index]
+            close_short_row = self.close_short_obj.loc[index]
+            if len(current_positions) > 0:
+                position_to_close = set({k: v for k,v in current_positions.items() if v['side'] == "LONG"}).intersection(set(close_long_row))
+                for pos in position_to_close:
+                    actual_row = self.df_list[pos].loc[index]
+                    close_price = actual_row['n1_ema_short']
+                    trade_result = (close_price - current_positions[pos]['price']) / current_positions[pos]['price']
+                    close_size = current_positions[pos]['size'] + current_positions[pos]['size']  * trade_result
+                    fee = close_size * maker_fee
+                    wallet += close_size - current_positions[pos]['size'] - fee
+                    usd_remaining += close_size - fee
+                    usd_pct_remaining += self.parameters_obj[pos]['wallet_exposure']
+                    trades.append({
+                        "pair": pos,
+                        "open_date": current_positions[pos]['date'],
+                        "close_date": index,
+                        "position": current_positions[pos]['side'],
+                        "open_reason": current_positions[pos]['reason'],
+                        "close_reason": "Limit",
+                        "open_price": current_positions[pos]['price'],
+                        "close_price": close_price,
+                        "open_fee": current_positions[pos]['fee'],
+                        "close_fee": fee,
+                        "open_trade_size":current_positions[pos]['size'],
+                        "close_trade_size":close_size,
+                        "wallet": wallet,
+                        "usd_remaining": usd_remaining
+                    })
+                    del current_positions[pos]   
+                short_position_to_close = set({k: v for k,v in current_positions.items() if v['side'] == "SHORT"}).intersection(set(close_short_row))
+                for pos in short_position_to_close:
+                    actual_row = self.df_list[pos].loc[index]
+                    close_price = actual_row['ema_short']
+                    trade_result = (current_positions[pos]['price'] - close_price) / current_positions[pos]['price']
+                    close_size = current_positions[pos]['size'] + current_positions[pos]['size'] * trade_result
+                    fee = close_size * maker_fee
+                    wallet += close_size - current_positions[pos]['size'] - fee
+                    usd_remaining += close_size - fee
+                    usd_pct_remaining += self.parameters_obj[pos]['wallet_exposure']
+                    trades.append({
+                        "pair": pos,
+                        "open_date": current_positions[pos]['date'],
+                        "close_date": index,
+                        "position": current_positions[pos]['side'],
+                        "open_reason": current_positions[pos]['reason'],
+                        "close_reason": "Limit",
+                        "open_price": current_positions[pos]['price'],
+                        "close_price": close_price,
+                        "open_fee": current_positions[pos]['fee'],
+                        "close_fee": fee,
+                        "open_trade_size":current_positions[pos]['size'],
+                        "close_trade_size":close_size,
+                        "wallet": wallet,
+                        "usd_remaining": usd_remaining
+                    })
+                    del current_positions[pos] 
+                    
+            # Buy
+            open_long_row = self.open_long_obj.loc[index]
+            if len(open_long_row) > 0:
+                for pos in open_long_row:
+                    if pos not in current_positions:
+                        actual_row = self.df_list[pos].loc[index]
+                        open_price = actual_row['n1_ema_short']
+                        pos_size = usd_remaining * (self.parameters_obj[pos]['wallet_exposure'] / usd_pct_remaining)
+                        usd_pct_remaining -= self.parameters_obj[pos]['wallet_exposure']
+                        fee = pos_size * maker_fee
+                        pos_size -= fee
+                        wallet -= fee
+                        usd_remaining -= pos_size+fee
+                        current_positions[pos] = {
+                            "size": pos_size,
+                            "date": index,
+                            "price": open_price,
+                            "fee":fee,
+                            "reason": "Limit",
+                            "side": "LONG"
+                        }
+            open_short_row = self.open_short_obj.loc[index]
+            if len(open_short_row) > 0:
+                for pos in open_short_row:
+                    if pos not in current_positions:
+                        actual_row = self.df_list[pos].loc[index]
+                        open_price = actual_row['n1_ema_short']
+                        pos_size = usd_remaining * (self.parameters_obj[pos]['wallet_exposure'] / usd_pct_remaining)
+                        usd_pct_remaining -= self.parameters_obj[pos]['wallet_exposure']
+                        fee = pos_size * maker_fee
+                        pos_size -= fee
+                        wallet -= fee
+                        usd_remaining -= pos_size+fee
+                        current_positions[pos] = {
+                            "size": pos_size,
+                            "date": index,
+                            "price": open_price,
+                            "fee":fee,
+                            "reason": "Limit",
+                            "side": "SHORT"
+                        }
+        df_days = pd.DataFrame(days)
+        df_days['day'] = pd.to_datetime(df_days['day'])
+        df_days = df_days.set_index(df_days['day'])
+
+        df_trades = pd.DataFrame(trades)
+        df_trades['open_date'] = pd.to_datetime(df_trades['open_date'])
+        df_trades = df_trades.set_index(df_trades['open_date'])   
+        
+        if return_type == "metrics":
+            return get_metrics(df_trades, df_days) | {
+                "wallet": wallet,
+                "trades": df_trades,
+                "days": df_days
+            }  
+        else:
+            return True
